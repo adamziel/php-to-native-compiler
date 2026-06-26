56532fd6c29636dc8cbe2b9312b8d0a7792bb59e
Adam Zieliński <adam@adamziel.com>
2026-06-26 20:31:28 +0200

    WIP: checkpoint (auto)

--- src/backend/runtime/arrays_access_and_iteration.c
+++ src/backend/runtime/arrays_access_and_iteration.c
@@ -980,8 +980,17 @@ static PTN_UNUSED PtnValue ptn_random_engine_new(
 static PTN_UNUSED int ptn_internal_class_name_is_closure(const char *class_name);
 static PTN_UNUSED int ptn_internal_class_name_is_curl_file(const char *class_name);
 static PTN_UNUSED int ptn_internal_class_name_is_directory(const char *class_name);
+static PTN_UNUSED int ptn_internal_class_name_is_phar(const char *class_name);
+static PTN_UNUSED int ptn_internal_class_name_is_phar_data(const char *class_name);
 static PTN_UNUSED int ptn_internal_class_name_is_php_token(const char *class_name);
 static PTN_UNUSED int ptn_internal_class_name_is_random_engine(const char *class_name);
+static PTN_UNUSED PtnValue ptn_phar_new(
+    PtnRuntime *runtime,
+    const char *class_name,
+    size_t argc,
+    const PtnValue *args,
+    size_t line
+);
 static PTN_UNUSED PtnValue ptn_curl_file_new(
     PtnRuntime *runtime,
     size_t argc,
