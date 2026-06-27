41e4f4e79bc502e45dc45cf55b2ef07310c3a76f
guard-105 <adam@adamziel.com>
2026-06-27 14:34:32 +0200

    Fix SOAP WSDL encoded scalar serialization

--- src/backend/runtime/internals_internal_functions.c
+++ src/backend/runtime/internals_internal_functions.c
@@ -155392,6 +155392,9 @@ static const char *ptn_soap_xsd_type_name(const char *type) {
     if (ptn_soap_type_name_is(type, "decimal")) {
         return "decimal";
     }
+    if (ptn_soap_type_name_is(type, "dateTime")) {
+        return "dateTime";
+    }
     if (ptn_soap_type_name_is(type, "base64Binary")) {
         return "base64Binary";
     }
@@ -155429,11 +155432,16 @@ static int ptn_soap_type_is_builtin_scalar(const char *type) {
         ptn_soap_type_name_is(type, "float") ||
         ptn_soap_type_name_is(type, "double") ||
         ptn_soap_type_name_is(type, "decimal") ||
+        ptn_soap_type_name_is(type, "dateTime") ||
         ptn_soap_type_name_is(type, "boolean") ||
         ptn_soap_type_name_is(type, "base64Binary") ||
         ptn_soap_type_name_is(type, "hexBinary") ||
         ptn_soap_type_name_is(type, "any") ||
         ptn_soap_type_name_is(type, "NMTOKENS");
+}
+
+static const char *ptn_soap_encoded_part_xsi_type_prefix(PtnSoapType *type, const char *part_type_local) {
+    return type == NULL && ptn_soap_type_is_builtin_scalar(part_type_local) ? "xsd" : "ns1";
 }
 
 static char *ptn_soap_resolved_scalar_type_dup(
@@ -156451,6 +156459,7 @@ static int ptn_soap_build_request(
     int literal = use != NULL && ptn_ascii_case_equal(use, "literal");
     int document = style != NULL && ptn_ascii_case_equal(style, "document");
     int soap_array_feature = ptn_soap_client_feature_enabled(data, 4);
+    int scalar_input_nil = argc != 0 && type == NULL && ptn_value_deref(args[0]).type == PTN_NULL;
     int simple_content_nil = 0;
     if (argc != 0 && type != NULL && type->has_simple_content) {
         PtnValue simple_value = ptn_null();
@@ -156546,7 +156555,8 @@ static int ptn_soap_build_request(
         );
     } else if ((type != NULL && type->is_simple && (type->is_list || type->is_union)) ||
                (type != NULL && !type->has_simple_content && !ptn_soap_type_has_element_fields(type) && ptn_soap_type_has_attribute_fields(type)) ||
-               simple_content_nil) {
+               simple_content_nil ||
+               scalar_input_nil) {
         ptn_string_buffer_append_format(
             &body,
             "<SOAP-ENV:Envelope xmlns:SOAP-ENV=\"http://schemas.xmlsoap.org/soap/envelope/\" xmlns:ns1=\"%s\" xmlns:xsi=\"http://www.w3.org/2001/XMLSchema-instance\" xmlns:xsd=\"http://www.w3.org/2001/XMLSchema\" xmlns:SOAP-ENC=\"http://schemas.xmlsoap.org/soap/encoding/\" SOAP-ENV:encodingStyle=\"http://schemas.xmlsoap.org/soap/encoding/\"><SOAP-ENV:Body>",
@@ -156625,8 +156635,17 @@ static int ptn_soap_build_request(
         } else {
             ptn_string_buffer_append_char(&body, '>');
         }
+    } else if (!literal && argc > 0 && ptn_value_deref(args[0]).type == PTN_NULL) {
+        ptn_string_buffer_append(&body, " xsi:nil=\"true\"/>");
+        open_part = 0;
     } else if (!literal) {
-        ptn_string_buffer_append_format(&body, " xsi:type=\"ns1:%s\">", part_type_local);
+        const char *xsi_prefix = ptn_soap_encoded_part_xsi_type_prefix(type, part_type_local);
+        ptn_string_buffer_append_format(
+            &body,
+            " xsi:type=\"%s:%s\">",
+            xsi_prefix,
+            strcmp(xsi_prefix, "xsd") == 0 ? ptn_soap_xsd_type_name(part_type_local) : part_type_local
+        );
     } else if (document && type != NULL && !type->is_simple &&
                !type->has_simple_content &&
                !ptn_soap_type_has_attribute_fields(type) &&
@@ -158812,6 +158831,10 @@ static void ptn_soap_append_response_value_xml(
     }
     ptn_string_buffer_append_char(buffer, '<');
     ptn_string_buffer_append(buffer, name == NULL ? "return" : name);
+    if (value.type == PTN_NULL) {
+        ptn_string_buffer_append(buffer, " xsi:nil=\"true\"/>");
+        return;
+    }
     if (value.type == PTN_OBJECT) {
         const char *type_name = ptn_soap_response_object_has_property(value, "varStruct")
             ? "SOAPStructStruct"
@@ -159455,12 +159478,15 @@ static void ptn_soap_emit_response(
         }
         ptn_string_buffer_append(&buffer, "<SOAP-ENV:Envelope xmlns:SOAP-ENV=\"http://schemas.xmlsoap.org/soap/envelope/\" xmlns:ns1=\"");
         ptn_xml_append_escaped_ex(&buffer, namespace_uri == NULL ? "" : namespace_uri, 1, 0);
-        ptn_string_buffer_append(&buffer, "\" xmlns:xsd=\"http://www.w3.org/2001/XMLSchema\" xmlns:SOAP-ENC=\"http://schemas.xmlsoap.org/soap/encoding/\" SOAP-ENV:encodingStyle=\"http://schemas.xmlsoap.org/soap/encoding/\"><SOAP-ENV:Body><ns1:");
+        ptn_string_buffer_append(&buffer, "\" xmlns:xsi=\"http://www.w3.org/2001/XMLSchema-instance\" xmlns:xsd=\"http://www.w3.org/2001/XMLSchema\" xmlns:SOAP-ENC=\"http://schemas.xmlsoap.org/soap/encoding/\" SOAP-ENV:encodingStyle=\"http://schemas.xmlsoap.org/soap/encoding/\"><SOAP-ENV:Body><ns1:");
         ptn_string_buffer_append(&buffer, method_name == NULL ? "" : method_name);
-        ptn_string_buffer_append(&buffer, "Response/></SOAP-ENV:Body></SOAP-ENV:Envelope>\n");
+        ptn_string_buffer_append(&buffer, "Response><");
+        ptn_string_buffer_append(&buffer, return_name == NULL ? "return" : return_name);
+        ptn_string_buffer_append(&buffer, " xsi:nil=\"true\"/></ns1:");
+        ptn_string_buffer_append(&buffer, method_name == NULL ? "" : method_name);
+        ptn_string_buffer_append(&buffer, "Response></SOAP-ENV:Body></SOAP-ENV:Envelope>\n");
         ptn_output_write(runtime, buffer.data, buffer.len);
         free(buffer.data);
-        (void)return_name;
         (void)line;
         return;
     }
